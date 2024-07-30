2a40cdf48c43a87471ec76b71b6c4da49d51dd81
Bidhun <bidhunvaikkath@gmail.com>
2024-07-30 17:03:16 +0530

    add a ref for handle input values

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+
+  const playerName = useRef();
+  const [enteredPlayerName , setEnteredPlayerName] = useState(''); 
+
+  function handleClick(){
+    setEnteredPlayerName(playerName.current.value)
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'uknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
